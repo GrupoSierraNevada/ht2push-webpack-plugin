9dfd8f1f43374c3ee20f72390c37675461c6c069
Juan Borda <quiasmo.paprika@gmail.com>
2018-08-31 11:06:58 -0500

    switch to jasmine because mocha was screwing with require()

--- test/html-js-css/test.js
+++ test/html-js-css/test.js
@@ -1,5 +1,3 @@
-const chai = require('chai');
-const expect = chai.expect;
 const Webpack = require("webpack");
 const MemoryFS = require("memory-fs");
 const config = require("./webpack.conf.js");
@@ -8,9 +6,9 @@ describe('basic html-js generate assets and .htaccess', function() {
     const fs = new MemoryFS();
     const compiler = Webpack(config);
     compiler.outputFileSystem = fs;
-    it('should produce exptected output files', function(done){
+    it("Should be null", function(done){
         compiler.run((err, stats)=>{
-            expect(err).to.be.null;
+            expect(err).toBe(null);
             done();
         });
     })
--- test/html-js-simlpe/test.js
+++ test/html-js-simlpe/test.js
@@ -1,5 +1,3 @@
-const chai = require('chai');
-const expect = chai.expect;
 const Webpack = require("webpack");
 const MemoryFS = require("memory-fs");
 const config = require("./webpack.conf.js");
@@ -8,22 +6,16 @@ describe('basic html-js generate assets and .htaccess', function() {
     const fs = new MemoryFS();
     const compiler = Webpack(config);
     compiler.outputFileSystem = fs;
-    it('should produce exptected output files', function(done){
+    it("should generate all files ", function(done){
         compiler.run((err, stats)=>{
-        
-            expect(err).to.be.null;
-            expect(fs.readdirSync("/").length ).to.equal(4);
-            expect(fs.existsSync("/main.js"), "/main.js doesnt exist").to.be.true;
-            expect(fs.existsSync("/out.html", "/out.html doesnt exist")).to.be.true;
-            expect(fs.existsSync("/out2.html", "/out2.html doesnt exist")).to.be.true;
-            expect(fs.existsSync("/htaccess.txt", "htaccess didnt emit")).to.be.true;
+            expect(err).toBe(null);
+            expect(fs.readdirSync("/").length ).toBe(4);
+            expect(fs.existsSync("/main.js"), "/main.js doesnt exist").toBe(true);
+            expect(fs.existsSync("/out.html", "/out.html doesnt exist")).toBe(true);
+            expect(fs.existsSync("/out2.html", "/out2.html doesnt exist")).toBe(true);
+            expect(fs.existsSync("/htaccess.txt", "htaccess didnt emit")).toBe(true);
+            expect(fs.readFileSync("/htaccess.txt", "utf-8")).toBe("bleh");
             done();
         });
     })
-
-    it("should have adequate basic .htaccess values", function(done){
-
-        expect(fs.readFileSync("/htaccess.txt", "utf-8")).to.equal("bleh");
-        done();
-    });
 });
